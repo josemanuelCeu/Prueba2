2942313cdf06197a0d8959135e65e81909169253
josemanuelordonez <jordonez5404@ceuandalucia.es>
2023-11-26 12:49:46 +0100

    Cambio

---
+++ Clase3_2.java
@@ -0,0 +1,13 @@
+package CEUTema3;
+
+public class Clase3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Bienvenido a Git Tema 3 Practica 2");  
+                System.out.print("Jose Manuel");
+
+
+	}
+
+}
